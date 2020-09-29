458da4eb6d3f7d8a4327c02bc3212f63c8fadada
Maria <mchumachkova.work@gmail.com>
2020-09-29 15:48:36 -0400

    added imgs and finished the project

---
+++ scripts/forecast.js
@@ -0,0 +1,26 @@
+const key = 'ROyaC83P6gOkVlEuRNM00AQDZRB9sRGG';
+
+
+//weather info
+const getWeather = async (id) => {
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch (base+query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+
+
+//city info
+
+const  getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+    const response = await fetch(base+query);
+    const data = await response.json();
+
+   return data[0];
+};
